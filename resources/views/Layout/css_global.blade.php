6817b5307ce65b9aea9af99e1b4a3e359456a7b7
mochoktas <tian.saktiawan@gmail.com>
2024-06-20 18:50:34 +0700

    update layout

---
+++ resources/views/Layout/css_global.blade.php
@@ -0,0 +1,14 @@
+<!-- Bootstrap Core Css -->
+<link href="{{asset('assets/plugins/bootstrap/css/bootstrap.css')}}" rel="stylesheet">
+
+<!-- Waves Effect Css -->
+<link href="{{asset('assets/plugins/node-waves/waves.css')}}" rel="stylesheet" />
+
+<!-- Animation Css -->
+<link href="{{asset('assets/plugins/animate-css/animate.css')}}" rel="stylesheet" />
+
+<!-- Custom Css -->
+<link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+
+<!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
+<link href="{{asset('assets/css/themes/all-themes.css')}}" rel="stylesheet" />
---
+++ resources/views/blank.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.main')
+
+@section('title_page','Blank')
+@section('title','Blank Page')
+@section('content')
+<h1>
+    Hello 
+</h1>
+@endsection
